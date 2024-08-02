9b07c8d7e9abfd9b4bae60e356619d322f29aa24
Spectra.codes by Drix10 <drixisgod@gmail.com>
2024-08-02 13:26:31 +0530

    generated: `events/ready.js`

---
+++ events/ready.js
@@ -0,0 +1,17 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity('Music 🎶', { type: 'LISTENING' });
+});
+
+module.exports = client;
